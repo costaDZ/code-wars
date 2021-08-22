371dea1f94c1f4af618847311206acf718e00ead
costaDZ <73201789+costaDZ@users.noreply.github.com>
2021-08-22 21:36:36 +0100

    make better solution 1

--- codeWars.js
+++ codeWars.js
@@ -34,7 +34,6 @@
 
 
 //===============================================================//
-
 // Trolls are attacking your comment section!
 // A common way to deal with this situation is to remove all of the vowels
 //from the trolls' comments, neutralizing the threat.
